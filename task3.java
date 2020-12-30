785d0fb034776550694048e45a797577c0977c82
Pattu22 <76771126+Pattu22@users.noreply.github.com>
2020-12-30 20:33:44 +0530

    Add files via upload

---
+++ task3.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class task3
+{
+     public static void main(String args[]) throws IOException
+	 {
+	   FileReader f1=new FileReader("F:\\file handling\\task3\\entry1.txt");
+	   FileReader f2=new FileReader("F:\\file handling\\task3\\entry2.txt");
+	   File f3=new File("F:\\file handling\\task3\\result.txt");
+	   
+	  BufferedReader br1=new BufferedReader
+	 }
+}
